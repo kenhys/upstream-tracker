235be1f316b8473f269e10ac8a2b12f66a029db5
HAYASHI Kentaro <kenhys@gmail.com>
2014-09-06 17:40:37 +0900

    Parse each component

--- lib/upstream/tracker/list.rb
+++ lib/upstream/tracker/list.rb
@@ -16,9 +16,33 @@ module Upstream
         html = fetch_index_html(options)
         doc = Nokogiri::HTML.parse(html[:data], nil, html[:charset])
         doc.xpath("//table[@id='Components']").each do |table|
+          components = {}
           table.xpath("tr").each do |tr|
             unless tr.attribute("id").value =~ /^(top|bottom)Header/
-              puts tr.attribute("id").value
+              label = tr.attribute("id").value
+              puts label
+              id = nil
+              html_file = nil
+              summary = nil
+              versions = nil
+              tr.xpath("td").each do |td|
+                case td.attribute("class").value
+                when "left"
+                  html_file = td.children.attribute("href").value
+                  id = html_file.sub(/versions\/(.+)\.html/, '\1')
+                when "summary"
+                  summary = td.children.text.sub(/ \(site, doc\)/, '')
+                when "results"
+                  versions = td.children.text
+                end
+                components[id] = {
+                  :id => id,
+                  :label => label,
+                  :html => html_file,
+                  :summary => summary,
+                  :versions => versions
+                }
+              end
             end
           end
         end
