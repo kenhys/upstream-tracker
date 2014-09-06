95db4cd491562864579c6af3c21cdcddaa132e4f
HAYASHI Kentaro <kenhys@gmail.com>
2014-09-06 13:50:11 +0900

    Add upstream-tracker command

--- lib/upstream/tracker.rb
+++ lib/upstream/tracker.rb
@@ -1,4 +1,5 @@
 require "upstream/tracker/version"
+require "upstream/tracker/command"
 
 module Upstream
   module Tracker
---
+++ lib/upstream/tracker/command.rb
@@ -0,0 +1,12 @@
+require 'thor'
+require 'upstream/tracker'
+
+module Upstream
+  module Tracker
+    class Command < Thor
+
+      #desc "list SUBCOMMAND", "List [TARGET]"
+      #subcommand "list", List
+    end
+  end
+end
