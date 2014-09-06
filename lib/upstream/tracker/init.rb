50d5680e304ba3aa094a50d15a495c69a4595008
HAYASHI Kentaro <kenhys@gmail.com>
2014-09-06 18:53:03 +0900

    Implement to fetch versions of specific component

--- lib/upstream/tracker/init.rb
+++ lib/upstream/tracker/init.rb
@@ -18,6 +18,35 @@ module Upstream
         end
       end
 
+      desc "library [LIBRARY]", "Initialize library"
+      option :list, :required => false
+      def library(arg = "")
+        if options.has_key?("list")
+          #p options
+          exit
+        end
+        exit unless arg
+        arg.split(/,/).each do |library|
+          components = load_component
+          if components.keys.include?(library)
+            fetch_versions_html(components[library])
+          end
+        end
+      end
+
+      private
+
+      def fetch_versions_html(component)
+        p component
+        url = UPSTREAM_TRACKER_URL + component[:html]
+        html = fetch_html(url)
+        path = File.join(get_config_dir, component[:html])
+        FileUtils.mkdir_p(File.dirname(path))
+        File.open(path, "w+") do |file|
+          file.puts(html[:data])
+        end
+      end
+
     end
   end
 end
