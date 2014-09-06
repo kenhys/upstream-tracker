6bd8c7c83bb848a57198427a7abfe6d8a68dd55e
HAYASHI Kentaro <kenhys@gmail.com>
2014-09-06 14:00:23 +0900

    Move placement of command class

--- lib/upstream/tracker.rb
+++ lib/upstream/tracker.rb
@@ -2,10 +2,20 @@ require "upstream/tracker/version"
 require "upstream/tracker/list"
 require "upstream/tracker/search"
 require "upstream/tracker/use"
-require "upstream/tracker/command"
 
 module Upstream
   module Tracker
-    # Your code goes here...
+    class Command < Thor
+
+      desc "list [SUBCOMMAND]", "List [TARGET]"
+      subcommand "list", List
+
+      desc "search SYMBOL", "Search symbol from http://upstream-tracker.org/."
+      subcommand "search", Search
+
+      desc "use LIBRARY", "Specify libraries which you want to use."
+      subcommand "use", Use
+
+    end
   end
 end
